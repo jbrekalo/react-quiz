8c45a4f3d4070e4aa36d764dd05ad42fdce1268f
jbrekalo <josip.brekalo@hotmail.com>
2023-10-21 23:57:48 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./Header";
+
+export default function App() {
+  return (
+    <div className="app">
+      <Header />
+
+      <main className="main">
+        <p>1/15</p>
+        <p>Question?</p>
+      </main>
+    </div>
+  );
+}
