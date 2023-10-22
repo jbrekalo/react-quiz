f51d7168662020224fcec709c599e9607574aa38
jbrekalo <josip.brekalo@hotmail.com>
2023-10-22 12:37:02 +0200

    fetching questions

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,63 @@
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
+import Main from "./Main";
+
+const initialState = {
+  question: [],
+
+  // 'loading', 'error', 'ready', 'active', 'finished'
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataRecieved":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+    default:
+      throw new Error("Action unknown");
+  }
+}
 
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(function () {
+    async function fetchQuestions() {
+      try {
+        const res = await fetch(`http://localhost:8000/questions`);
+        if (!res.ok)
+          throw new Error("Something went wrong with fetching questions");
+
+        const data = await res.json();
+        if (data.Response === "False") throw new Error("Question not found");
+
+        console.log(data);
+        dispatch({ type: "dataRecieved", payload: data });
+      } catch (err) {
+        console.log(err.message);
+        dispatch({ type: "dataFailed" });
+      }
+    }
+    fetchQuestions();
+  }, []);
+
   return (
     <div className="app">
       <Header />
 
-      <main className="main">
+      <Main>
         <p>1/15</p>
         <p>Question?</p>
-      </main>
+      </Main>
     </div>
   );
 }
