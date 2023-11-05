a76e8ff1889bb05e6d0e9537062058d7aaed0938
jbrekalo <josip.brekalo@hotmail.com>
2023-11-05 21:19:59 +0100

    context api

--- src/components/FinishScreen.jsx
+++ src/components/FinishScreen.jsx
@@ -1,7 +1,10 @@
-function FinishScreen({ points, maxPossiblePoints, highscore, dispatch }) {
-  const percentage = (points / maxPossiblePoints) * 100;
+import { useQuiz } from "../contexts/QuizContext";
 
+function FinishScreen() {
+  const { points, highscore, maxPossiblePoints, dispatch } = useQuiz();
+  const percentage = (points / maxPossiblePoints) * 100;
   let emoji;
+
   if (percentage === 100) emoji = "🏅";
   if (percentage >= 80 && percentage < 100) emoji = "🎉";
   if (percentage >= 50 && percentage < 80) emoji = "🙃";
